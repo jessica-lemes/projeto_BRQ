973653d6690d0bc6656ca877c6c5061503a4ecaa
Gabriel-Alves-Dev <83257171+Gabriel-Alves-Dev@users.noreply.github.com>
2021-05-06 00:39:17 -0300

    teste de correção

--- app/src/main/java/com/example/app_brq/UI/PrincipalActivity.kt
+++ app/src/main/java/com/example/app_brq/UI/PrincipalActivity.kt
@@ -2,10 +2,11 @@ package com.example.app_brq.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_brq.R
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        setContentView(R .layout.activity_principal)
     }
 }
