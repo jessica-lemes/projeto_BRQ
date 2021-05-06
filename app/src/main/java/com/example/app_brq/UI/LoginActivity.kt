9f7a70e33da2d0bd718d85cd3b10c091fdd07efc
Gabriel-Alves-Dev <83257171+Gabriel-Alves-Dev@users.noreply.github.com>
2021-05-06 00:31:02 -0300

    criando objetos padroes

--- app/src/main/java/com/example/app_brq/UI/LoginActivity.kt
+++ app/src/main/java/com/example/app_brq/UI/LoginActivity.kt
@@ -1,11 +1,13 @@
-package com.example.app_brq
+package com.example.app_brq.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_brq.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        var ola = 1
     }
 }
