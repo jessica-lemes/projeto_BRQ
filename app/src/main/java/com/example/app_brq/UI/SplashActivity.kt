86a3337cbedbce0c69812798ce65e2afd76f18b4
Gabriel-Alves-Dev <83257171+Gabriel-Alves-Dev@users.noreply.github.com>
2021-05-10 19:50:21 -0300

    inclusao do background

--- app/src/main/java/com/example/app_brq/UI/SplashActivity.kt
+++ app/src/main/java/com/example/app_brq/UI/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.example.app_brq
+package com.example.app_brq.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_brq.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
