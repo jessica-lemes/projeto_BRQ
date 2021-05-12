0525a6d35ce00582381cc9f6cfe98b06bc32c42d
Gabriel-Alves-Dev <83257171+Gabriel-Alves-Dev@users.noreply.github.com>
2021-05-11 21:39:56 -0300

    config tela cadastro realizada

--- app/src/main/java/com/example/app_brq/UI/LoginActivity.kt
+++ app/src/main/java/com/example/app_brq/UI/LoginActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import com.example.app_brq.R
 
 class LoginActivity : AppCompatActivity() {
@@ -19,21 +18,28 @@ class LoginActivity : AppCompatActivity() {
         carregarEventos()
     }
     private fun carregarElementos() {
-        campoEmail = findViewById(R.id.inputEmail)
-        campoSenha = findViewById(R.id.inputSenha)
+        campoEmail = findViewById(R.id.inputEmailCad)
+        campoSenha = findViewById(R.id.inputSenhaCad)
         botaoEnviar = findViewById(R.id.btnLogin)
     }
     private fun carregarEventos() {
         botaoEnviar.setOnClickListener {
             val email =  campoEmail.text.toString()
             val senha = campoSenha.text.toString()
-            if(email != "Gabriel" && email != "Jessica" && email != "Flavia"){
-                val intent = Intent(this,CadastroActivity::class.java)
-                startActivity(intent)
-            }else{
+            if(email == "Gabriel" && senha == "Gabriel"){
                 val intent = Intent(this,PrincipalActivity::class.java)
                 startActivity(intent)
+            }else if(email == "Jessica" && senha == "Jessica"){
+                val intent = Intent(this,PrincipalActivity::class.java)
+                startActivity(intent)
+            }else if (email == "Flavia" && senha == "Flavia"){
+                val intent = Intent(this,PrincipalActivity::class.java)
+                startActivity(intent)
+            }else{
+                val intent = Intent(this,CadastroActivity::class.java)
+                startActivity(intent)
             }
+
         }
     }
 }
