c3aaa02408f811d2e9b3401ca6cc11d737135858
jessica-lemes <jessica.costa.moraes@gmail.com>
2021-05-12 18:02:14 -0300

    layout da tela principal finalizada

--- app/src/main/java/com/example/app_brq/UI/PrincipalActivity.kt
+++ app/src/main/java/com/example/app_brq/UI/PrincipalActivity.kt
@@ -2,11 +2,20 @@ package com.example.app_brq.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.app_brq.R
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R .layout.activity_principal)
+    }
+
+    fun adicionarReceita(view: View){
+        //abrir tela de receita
+    }
+
+    fun adicionarDespesa(view: View){
+        //abrir tela de despesa
     }
 }
