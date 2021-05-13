f13e8ba59be431121166e88e21669565c4fd645b
jessica-lemes <jessica.costa.moraes@gmail.com>
2021-05-13 17:55:27 -0300

    Layouts das telas de despesa e receita finalizadas

---
+++ app/src/main/java/com/example/app_brq/UI/DespesasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_brq.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app_brq.R
+
+class DespesasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_despesas)
+    }
+}
